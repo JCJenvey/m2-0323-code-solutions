037737d5b85f81d60ccfae9155ac78d7f3c2b6bb
Jared Jenvey <jared@exit-12.com>
2023-03-21 03:47:22 +0000

    Completed javascript-this exercise

---
+++ javascript-this/student.js
@@ -0,0 +1,14 @@
+/* exported student */
+
+const student = {
+  firstName: 'Jared',
+  lastName: 'Jenvey',
+  subject: 'JavaScript',
+  getFullName: function () {
+    return this.firstName + ' ' + this.lastName;
+  },
+  getIntroduction: function () {
+    return 'Hello, my name is ' + this.getFullName() +
+    ' and I am studying ' + this.subject + '.';
+  }
+};
