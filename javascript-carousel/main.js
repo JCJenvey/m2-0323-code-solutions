70e42cd205a6732c60839b740d99eb14e73e54d4
Jared Jenvey <jared@exit-12.com>
2023-04-02 01:13:22 +0000

    arrows now move carousel forwards or backwards

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -1,21 +1,39 @@
 const $img = document.getElementsByClassName('img');
 const $dot = document.getElementsByClassName('dot');
+const $forwardArrow = document.querySelector('.forward');
+const $backArrow = document.querySelector('.back');
 
-/* const imgViewID = */setInterval(function ($img) {
+let imgViewID = setInterval(imgIntervalCallback, 3000);
+
+// Eventy listeners below:
+$forwardArrow.addEventListener('click', function (e) {
+  clearInterval(imgViewID);
+  const currentImg = advanceImg();
+  imgSwap($img, $dot, currentImg);
+  imgViewID = setInterval(imgIntervalCallback, 3000);
+});
+
+$backArrow.addEventListener('click', function (e) {
+  clearInterval(imgViewID);
   let currentImg;
   for (let i = 0; i < $img.length; i++) {
     if ($img[i].getAttribute('class') === 'img') {
-      if (i === $img.length - 1) {
-        currentImg = $img[0].getAttribute('data-img');
+      if (i === 0) {
+        currentImg = $img[$img.length - 1].getAttribute('data-img');
       } else {
-        currentImg = $img[i + 1].getAttribute('data-img');
+        currentImg = $img[i - 1].getAttribute('data-img');
       }
     }
   }
   imgSwap($img, $dot, currentImg);
-}, 3000, $img, $dot);
+  imgViewID = setInterval(imgIntervalCallback, 3000);
+});
 
 // Function declarations below
+function imgIntervalCallback() {
+  const currentImg = advanceImg();
+  imgSwap($img, $dot, currentImg);
+}
 
 function imgSwap($img, $dot, currentImg) {
   for (let i = 0; i < $img.length; i++) {
@@ -27,4 +45,18 @@ function imgSwap($img, $dot, currentImg) {
       $dot[i].className = 'fa-regular fa-circle dot';
     }
   }
+}
+
+function advanceImg() {
+  let currentImg;
+  for (let i = 0; i < $img.length; i++) {
+    if ($img[i].getAttribute('class') === 'img') {
+      if (i === $img.length - 1) {
+        currentImg = $img[0].getAttribute('data-img');
+      } else {
+        currentImg = $img[i + 1].getAttribute('data-img');
+      }
+    }
+  }
+  return currentImg;
 }
