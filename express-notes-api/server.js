b292e4fa1a467a28d6ecd0f49ddcf1a330b7f6e8
Jared Jenvey <jared@exit-12.com>
2023-05-14 20:31:10 +0000

    Client can GET a single note by id

--- express-notes-api/server.js
+++ express-notes-api/server.js
@@ -4,7 +4,7 @@ import { readFile } from 'node:fs/promises';
 const app = express();
 
 const notesData = JSON.parse(await readFile('./data.json', { encoding: 'utf8' }));
-// const nextID = notesData.nextId;
+// let nextID = notesData.nextId;
 const notes = notesData.notes;
 
 app.get('/api/notes', (req, res) => {
@@ -13,6 +13,16 @@ app.get('/api/notes', (req, res) => {
     notesArr.push(notes[note]);
   }
   res.json(notesArr);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+  if (parseInt(req.params.id) < 1 || !parseInt(req.params.id)) {
+    res.status(400).json({ error: 'id must be a positive integer' });
+  } else if (notes[req.params.id]) {
+    res.json(notes[req.params.id]);
+  } else {
+    res.status(404).json({ error: 'cannot find note with id ' + req.params.id });
+  }
 });
 
 app.listen(8080, () => {
